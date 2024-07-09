3b32834648e2a343b7c313974f11831e7a0beeeb
Tristan Bridges <mophead4@gmail.com>
2024-07-09 15:57:21 -0500

    qr on about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,10 +8,12 @@ const About = () => {
         >
           About Showcase
         </h1>
+       
+        
         <div className="card mt-5 pb-10 mx-40 pt-5 bg-base-300">
           <div className="card-body items-center" style={{ fontFamily: "Chillax" }}>
-            <div className="card bg-base-200 w-1/2">
-            <h1 className="text-lg text-center">
+            <div className="card bg-base-200 w-1/2 mb-5">
+            <h1 className="text-xl text-center">
               Showcase is a platform for small to midsize venues on the same
               circuit to share data about bands they've hosted. This will
               provide access to analytics on ticket sales & alcohol sales volume
@@ -19,9 +21,9 @@ const About = () => {
               visited. This allows venues to tailor their expenditures accordingly. 
             </h1>
             </div>
-            <div className="card bg-base-200 w-1/2 items-center">
+            <div className="card bg-base-200 w-1/2 items-center mb-5">
                 <h1 className="card-title">Coming Soon</h1>
-                <ul className="text-center">
+                <ul className="text-center text-lg">
                     <li>Mobile Formatting</li>
                     <li>Search Functionality</li>
                     <li>Ability to filter data over time</li>
@@ -29,7 +31,7 @@ const About = () => {
             </div>
             <div className="card bg-base-200 w-1/2 items-center">
                 <h1 className="card-title">Technologies Used</h1>
-                <ul className="text-center">
+                <ul className="text-center text-lg">
                     <li>React</li>
                     <li>Redux/Redux-Sagas</li>
                     <li>TailwindCSS/DaisyUI</li>
@@ -38,7 +40,15 @@ const About = () => {
                     <li>csvtojson</li>
                 </ul>
             </div>
-          </div>
+            <div className="card bg-base-200 w-1/2 items-center">
+            <div className="card-body">
+                <img src="/adobe-express-qr-code.png" alt="" />
+            </div>
+        </div>
+            </div>
+          
+        
+       
         </div>
       </div>
     </>
