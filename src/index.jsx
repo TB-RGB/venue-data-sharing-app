5467610b7be28cceddd2e523e6f3cadefb505240
Tristan Bridges <mophead4@gmail.com>
2024-07-12 16:11:21 -0500

    font fixing

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { CustomProvider } from "rsuite";
+import "./index.css";
 
 import store from "./redux/store";
 
